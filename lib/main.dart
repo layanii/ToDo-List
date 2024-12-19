690dcc5afb917a50611b4811526ff1893e3ec4d7
layanii <lyanksa66@gmail.com>
2024-12-19 14:50:20 +0300

    main change

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,16 @@ import 'package:todolist/providers/task_provider.dart';
 import 'package:todolist/screens/todo_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ToDoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(create:  (context) => TaskProvider(),
-    child: const MaterialApp(
-      home: TodoScreen()));
+    return ChangeNotifierProvider(
+        create: (context) => TaskProvider(),
+        child: const MaterialApp(home: TodoScreen()));
   }
 }
